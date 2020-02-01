4446ff05af53ad6b2f220487318ceee5dbbe8dcb
Diego <izayakazuto97@gmail.com>
2020-02-01 10:48:55 -0300

    Create numeros-impares.php

---
+++ numeros-impares.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+for($i = 1; $i < 100; $i++)
+{
+    if ($i % 2 != 0)
+    { 
+        echo $i . PHP_EOL; //Irá exibir todos os números que resto da divisão for diferente de 0, ou seja os números ímpares.
+    }
+}
